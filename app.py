f787c7fc20171d3c6177937678b9a1b66f276070
Aadarsh Kannan <aadarshkannan111@gmail.com>
2022-07-21 06:56:15 +0000

    Main app

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.header("Building projects with streamlit")
+st.info("More projects to come! Under construction!!!")
+st.image("https://media.giphy.com/media/hvN3SkNMRSB7mZa8JL/giphy.gif")
+st.success("QR Generator App Done 👈")
