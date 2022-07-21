f1f9613e965734466dad9e83e88e91bd08d41f19
Aadarsh Kannan <aadarshkannan111@gmail.com>
2022-07-21 08:33:39 +0000

    Improved a bit

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 import streamlit as st
+
 st.header("Building projects with streamlit")
 st.info("More projects to come! Under construction!!!")
 st.image("https://media.giphy.com/media/hvN3SkNMRSB7mZa8JL/giphy.gif")
-st.success("QR Generator App Done 👈")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    with st.expander("Completed", expanded = True): 
+        st.success("1. QR Generator App")
+
+with col2:
+    with st.expander("TO-DO", expanded = True):
+        st.info("2. Weather App")
+        st.info('3. Translation App')
+        st.info('4. Geolocation App')
+        st.info('5. Bored App')
