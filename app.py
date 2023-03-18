7700d2a87bb4abf765f32dd7848da69bd852b167
Aadarsh Kannan <aadarshkannan111@gmail.com>
2023-03-18 16:05:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ st.header("Building projects with streamlit")
 st.info("More projects to come! Under construction!!!")
 st.image("https://media.giphy.com/media/hvN3SkNMRSB7mZa8JL/giphy.gif")
 
-with st.sidebar:
-    with st.expander("Socials"):
-            st.markdown('''
+with st.expander("Socials"):
+        st.markdown('''
             [![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/dotaadarsh)
             [![Twitter](https://img.shields.io/badge/twitter-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white)](https://twitter.com/dotaadarsh)
             [![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/dotaadarsh/)
@@ -17,6 +16,17 @@ with st.sidebar:
             [![Discord](https://img.shields.io/badge/Discord-%237289DA.svg?style=for-the-badge&logo=discord&logoColor=white)](https://discord.com/invite/Jj8xeWpnEe)
             [![Spotify](https://img.shields.io/badge/Spotify-1ED760?style=for-the-badge&logo=spotify&logoColor=white)](https://open.spotify.com/user/w4vmhygkyyzefhe1u3bpqrlo6)
             ''')
-    with st.expander('Buy me a coffee'):
-     components.html('''
-    <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="aadarshk" data-color="#5F7FFF" data-emoji=""  data-font="Lato" data-text="Buy me a coffee" data-outline-color="#000000" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script>''',height = 100)
+with st.expander('Buy me a coffee'):
+        components.html('''
+                <script 
+                type="text/javascript" 
+                src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" 
+                data-name="bmc-button" 
+                data-slug="aadarshk" 
+                data-color="#5F7FFF" 
+                data-emoji=""  
+                data-font="Lato" 
+                data-text="Buy me a coffee" 
+                data-outline-color="#000000" 
+                data-font-color="#ffffff" 
+                data-coffee-color="#FFDD00" ></script>''', height=100)
