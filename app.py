bdb9cbe8f28dac49f6a2ccfe4b546f213cef1f15
Aadarsh Kannan <aadarshkannan111@gmail.com>
2023-03-24 18:19:23 +0000

    Update README.md

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Streamlit projects", page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+st.set_page_config(page_title="Streamlit projects", page_icon=None,
+                   layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 st.header("Building projects with streamlit")
 st.info("More projects to come! Under construction!!!")
 st.image("https://media.giphy.com/media/hvN3SkNMRSB7mZa8JL/giphy.gif")
 
+st.markdown("""
+| Title | Blog |
+|-------------------------------------------|:----------------------------------------------------------------------------------------:|
+| You can just turn data scripts into apps! | [↗](https://aadarshkannan.hashnode.dev/you-can-just-turn-data-scripts-into-apps) |
+| Building QR Code generator | [↗](https://aadarshkannan.hashnode.dev/building-qr-code-generator) |
+| #01 Lets create a simple weather App | [↗](https://aadarshkannan.hashnode.dev/lets-create-a-simple-weather-app-part-1) |
+| real-time live dashboard | [↗](https://blog.streamlit.io/how-to-build-a-real-time-live-dashboard-with-streamlit/) |
+
+""")
+
 with st.expander("Socials"):
-        st.markdown('''
+    st.markdown('''
             [![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)](https://github.com/dotaadarsh)
             [![Twitter](https://img.shields.io/badge/twitter-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white)](https://twitter.com/dotaadarsh)
             [![LinkedIn](https://img.shields.io/badge/linkedin-%230077B5.svg?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/dotaadarsh/)
@@ -17,7 +28,7 @@ with st.expander("Socials"):
             [![Spotify](https://img.shields.io/badge/Spotify-1ED760?style=for-the-badge&logo=spotify&logoColor=white)](https://open.spotify.com/user/w4vmhygkyyzefhe1u3bpqrlo6)
             ''')
 with st.expander('Buy me a coffee'):
-        components.html('''
+    components.html('''
                 <script 
                 type="text/javascript" 
                 src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" 
